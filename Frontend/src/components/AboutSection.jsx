1dfa20fb988be32959a2ee144d05f71c9558cde9
Shashikant Shirsath <122335452+ShashikantShirsath@users.noreply.github.com>
2026-01-28 15:29:56 +0530

    Update CV link to point to GitHub repository

--- Frontend/src/components/AboutSection.jsx
+++ Frontend/src/components/AboutSection.jsx
@@ -29,7 +29,7 @@ const AboutSection = () => {
                 {" "}
                 Get In Touch
               </a>
-              <a href="Frontend\public\resume\Shashikant_resume.pdf" className='px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300' download={true}>
+              <a href="https://github.com/ShashikantShirsath/ShashikantShashikant-Portfolio/blob/main/Frontend/public/resume/Shashikant_resume.pdf" className='px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300' download={true}>
                 Download CV
               </a>
             </div>
